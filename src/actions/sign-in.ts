c667bc1f55bb847bf9bdc921df2cbe1df97640d1
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-12 18:35:26 -0700

    - Add prepared server actions for each of the paths planned. - Reorganise signIn and signOut functions in their own files. - Export all individual functions from actions/index.ts

---
+++ src/actions/sign-in.ts
@@ -0,0 +1,8 @@
+'use server';
+
+import * as auth from '@/auth';
+
+export async function signIn() {
+    // whenever we call, we pass in a string indicating what provider we want to sign in with
+    return auth.signIn('github');
+}
