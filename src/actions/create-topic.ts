1aed697cf47826129a28643aec9d7f73687cb502
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-17 11:02:38 -0700

    - Created formState for error validation - Defined CreateTopicFormState interface to coordinate types across useActionState and createTopic function

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -5,15 +5,34 @@ import { z } from 'zod';
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase letters or dashes without spaces"}),
     description: z.string().min(10),
-})
+});
 
-export async function createTopic(formData: FormData) {
+interface CreateTopicFormState {
+    errors: {
+        name?: string[];
+        description?: string[];
+    }
+}
+
+// the type of the formState argument must match the type that we set in useActionState (in TopicCreateForm) and the return value of the component function
+// in order to returns a value that satisfies the CreateTopicFormState interface, we can set a declared type return annotation.
+// we wrap that return annotation in a Promise because this is an async function.
+export async function createTopic(
+    formState: CreateTopicFormState,
+    formData: FormData
+    ): Promise<CreateTopicFormState> {
     const result = createTopicSchema.safeParse({
         name: formData.get('name'),
         description: formData.get('description')
     });
 
     if (!result.success) {
-        console.log(result.error.flatten().fieldErrors);
+        return {
+            errors: result.error?.flatten().fieldErrors,
+        };
     }
+
+    return {
+        errors: {}
+    };
 }
