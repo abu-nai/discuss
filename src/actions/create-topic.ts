9afd3270318e09518e5212360e1c9763f26191fa
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-17 13:22:32 -0700

    - Updated CreateTopicFormState interface to include general form errors - Updated TopicCreateForm to display _form errors if present

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { z } from 'zod';
+import { auth } from '@/auth';
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/^[a-z-]+$/, { message: "Must be lowercase letters or dashes without spaces"}),
@@ -11,6 +12,7 @@ interface CreateTopicFormState {
     errors: {
         name?: string[];
         description?: string[];
+        _form?: string[];
     }
 }
 
@@ -21,18 +23,27 @@ export async function createTopic(
     formState: CreateTopicFormState,
     formData: FormData
     ): Promise<CreateTopicFormState> {
-    const result = createTopicSchema.safeParse({
-        name: formData.get('name'),
-        description: formData.get('description')
-    });
+        const result = createTopicSchema.safeParse({
+            name: formData.get('name'),
+            description: formData.get('description')
+        });
 
-    if (!result.success) {
+        if (!result.success) {
+            return {
+                errors: result.error?.flatten().fieldErrors,
+            };
+        }
+
+        const session = await auth();
+        if (!session || !session.user) {
+            return {
+                errors: {
+                    _form: ['You must be signed in to do this.']
+                }
+            };
+        }
+
         return {
-            errors: result.error?.flatten().fieldErrors,
+            errors: {}
         };
-    }
-
-    return {
-        errors: {}
-    };
 }
