dff69a559422df7f208024ace1422c706cc90c45
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-21 09:45:57 -0700

    - Updated create-post.ts to validate if a user-searched post exists

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async function TopicShowPage({ params }: TopicShowPageProps) {
         </div>
 
         <div>
-            <PostCreateForm />
+            <PostCreateForm slug={slug}/>
         </div>
     </div>
   )
