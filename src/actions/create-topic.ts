4bf4ab66cb38cb1dff36bf1f86596f023729fcce
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-21 09:17:53 -0700

    - Implemented validation for title and content length.

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -23,7 +23,7 @@ interface CreateTopicFormState {
 }
 
 // the type of the formState argument must match the type that we set in useActionState (in TopicCreateForm) and the return value of the component function
-// in order to returns a value that satisfies the CreateTopicFormState interface, we can set a declared type return annotation.
+// in order to return a value that satisfies the CreateTopicFormState interface, we can set a declared type return annotation.
 // we wrap that return annotation in a Promise because this is an async function.
 export async function createTopic(
     formState: CreateTopicFormState,
