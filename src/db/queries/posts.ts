6e5bc6d1c76f64832e6d533fe5a738319ea2b297
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-25 16:28:00 -0700

    - Reformatted code in topic-create-form.tsx - Added comment to posts.ts

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -9,6 +9,12 @@ export type PostWithData = (
     }
 )
 
+// Automating the generation of our type: This type looks at what gets returned by the type of fetchPostsByTopicSlug. It then unwraps that (what the promise gets resolved with). Because our promise is resolved with an array of post objects, [number] means "take the type of a single element in that array."
+// This approach does not scale well if we begin to add additional queries to the function.
+// export type PostWithData = Awaited<
+//     ReturnType<typeof fetchPostsByTopicSlug>
+// >[number];
+
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
     return db.post.findMany({
         where: {topic: { slug }},
