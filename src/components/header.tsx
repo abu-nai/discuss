6203b713c5626fbb8b92fc6b0a2acadeca3db153
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-04-26 16:38:31 -0700

    - Created search.ts server action and exported from index.ts for use in - Created search/page.tsx to search and display posts with given search term - Updated search-input.tsx component to populate search term in input bar once searched - Updated posts.ts with fetchPostsBySearchTerm query to retrieve array of posts using user search term

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Suspense } from 'react';
 import {
     Navbar,
     NavbarBrand,
@@ -17,7 +18,9 @@ export default function Header() {
             </NavbarBrand>
             <NavbarContent justify="center">
                 <NavbarItem>
-                    <SearchInput />
+                    <Suspense>
+                        <SearchInput />
+                    </Suspense>
                 </NavbarItem>
             </NavbarContent>
 
--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -9,6 +9,22 @@ export type PostWithData = (
     }
 )
 
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+    return db.post.findMany({
+        include: {
+            topic: { select: { slug: true } },
+            user: { select: { name: true, image: true } },
+            _count: { select: { comments: true } }
+        },
+        where: {
+            OR: [
+                { title: { contains: term } },
+                { content: { contains: term } }
+            ]
+        }
+    })
+}
+
 // Automating the generation of our type: This type looks at what gets returned by the type of fetchPostsByTopicSlug. It then unwraps that (what the promise gets resolved with). Because our promise is resolved with an array of post objects, [number] means "take the type of a single element in that array."
 // This approach does not scale well if we begin to add additional queries to the function.
 // export type PostWithData = Awaited<
