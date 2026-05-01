a4c9c7b3d18747fd5f0548a28f2aa4074721da3a
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-05-01 12:42:11 -0700

    - Made some minor updates to personalise this project

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -10,7 +10,7 @@ export default function SearchInput() {
     // defaultValue expects to receive either a string or UNDEFINED. searchParams.get('term') will return either a string or NULL. we add || '' to our defaultValue to satisfy its expected values.
     return (
         <form action={actions.search}>
-            <Input defaultValue={searchParams.get('term') || '' } name="term" />
+            <Input placeholder="Looking for something?" defaultValue={searchParams.get('term') || '' } name="term" />
         </form>
         )
 }
