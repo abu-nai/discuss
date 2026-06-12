ad41ebbf02fc7f0aaab357f2ad99f8e7abd37a5c
abu-nai <119476958+abu-nai@users.noreply.github.com>
2026-06-12 16:08:27 -0700

    Things learned while getting this test to work: - Remember to provide controlled data so that we know exactly what to expect and test for. - To avoid using the "any" type in a test, the mock data must match the type exactly, which is what makes TypeScript so useful for catching bugs during the programming process. - Since PostList is an async Server Component, the function itself is async and returns a Promise of JSX rather than JSX directly. RTL's render() expects synchronous JSX (not a Promise), so I need to await the component function first to get the resolved JSX output, then pass that to render().

--- src/components/posts/post-list.test.tsx
+++ src/components/posts/post-list.test.tsx
@@ -1,19 +1,37 @@
 import { render, screen } from '@testing-library/react';
 import PostList from './post-list';
 
-test('postlist will display post title, author, and number of comments', () => {
-    render(<PostList fetchData={() => {
-        return (Promise<[
-            {
-                topic: { slug: "test-slug" },
-                user: { name: "pluto" },
-                _count: { comments: 4 }
-            }]
-        >)
-    }
-    }/>);
+test('postlist will display post title, author, and number of comments', async () => {
+    // Provide controlled data:
+    const mockPosts = [
+        {
+            id: '1',
+            title: 'First Test Post',
+            content: 'Testing, testing, 1, 2, 3',
+            topic: { slug: 'rehearsal' },
+            user: { name: 'Pluto' },
+            _count: { comments: 10 },
+        },
+        {
+            id: '2',
+            title: 'Second Test Post',
+            content: 'Is this thing even on?',
+            topic: { slug: 'rehearsal' },
+            user: { name: 'Fern' },
+            _count: { comments: 6 },
+        },
+    ];
 
-    
+    // Isolate PostList's awaited output and render directly since PostList is an async Server Component.
+    const Component = await PostList({ fetchData: async () => mockPosts as any});
+
+    render(Component);
+
+    expect(screen.getByText('First Test Post')).toBeInTheDocument();
+    expect(screen.getByText('Second Test Post')).toBeInTheDocument();
+    expect(screen.getByText(/by pluto/i)).toBeInTheDocument();
+    expect(screen.getByText(/by fern/i)).toBeInTheDocument();
+    expect(screen.getByText(/10 comments/i)).toBeInTheDocument();
 
 
 });
